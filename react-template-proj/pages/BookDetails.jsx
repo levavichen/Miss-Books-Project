4c8c821631d3a8556ed213fd300774809ddc0dc7
chen <levavi.chen@gmail.com>
2024-09-16 20:30:16 +0300

    add txt based on data

--- react-template-proj/pages/BookDetails.jsx
+++ react-template-proj/pages/BookDetails.jsx
@@ -23,18 +23,43 @@ export function BookDetails({ onBack, bookId }) {
         pageCount, categories, thumbnail, language,
         listPrice: { amount, currencyCode, isOnSale } } = book
 
+    function getTypeOfReading() {
+        if (pageCount > 500) return 'Serious Reading'
+        if (pageCount < 500 && pageCount > 200) return 'Descent Reading'
+        if (pageCount < 100) return 'Light Reading'
+        return ''
+    }
+
+    function getTypeByPublishTime() {
+        if ((2024 - publishedDate) > 10) return 'Vintage'
+        if ((2024 - publishedDate) <= 1) return 'New'
+        return ''
+    }
+
+    function getAmountClass() {
+        if (amount > 100) return 'red'
+        if (amount < 20) return 'green'
+        return ''
+    }
+
+
+
+
+    const typeOfReading = getTypeOfReading()
+    const typeByPublishTime = getTypeByPublishTime()
+    const amountClass = getAmountClass()
 
     return (
         <section className="book-details">
-            <h1>Title: {title}</h1>
-            <h1>Book Price: {amount}</h1>
-            <p>Subtitle: {subtitle}</p>
+            <h1>{title}</h1>
+            <h3>Book Price: <span className={`amount ${amountClass}`}>{amount}</span></h3>
             <p>Authors: {authors}</p>
-            <p>Language: {language}</p>
-            <p>Publish Date: {publishedDate}</p>
-            <p>Page Count: {pageCount}</p>
-            <p>Book Categories: {categories}</p>
             <p>Description: {description}</p>
+            <p>Subtitle: {subtitle}</p>
+            <p>Language: {language}</p>
+            <p>Book Categories: {categories}</p>
+            <p>{typeOfReading}</p>
+            <p>{typeByPublishTime}</p>
             <img src={thumbnail} alt="book-image" />
             <button onClick={onBack}>Back</button>
         </section>
